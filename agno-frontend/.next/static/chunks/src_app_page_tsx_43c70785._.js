dc351858a78f8b4b624dfae7a0131406206fc7dc
costa-gHs <henriquedelli2020@gmail.com>
2025-08-01 19:57:20 -0300

    Correções integração

--- agno-frontend/.next/static/chunks/src_app_page_tsx_43c70785._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_9ae545a4._.js",
-  "static/chunks/node_modules_recharts_es6_6a6071f7._.js",
-  "static/chunks/node_modules_d318f8d9._.js"
-],
-    source: "dynamic"
-});
